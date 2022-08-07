3cb051cdbcb5e702975b75b2bc1804fa61942cc9
Luana-RodriguesdaSilva <106934139+Luana-RodriguesdaSilva@users.noreply.github.com>
2022-08-06 22:24:52 -0300

    adicionando dois console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação se conectando ao banco de dados")
-
+console.log("O paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o github")
